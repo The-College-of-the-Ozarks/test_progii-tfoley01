ebfb863149d1dec9613e6cac4b132b6b560c9eac
Tom Foley <110187648+tfoley01@users.noreply.github.com>
2022-11-29 15:58:47 +0000

    Completed the assignment and formatted the code.

--- main.java
+++ main.java
@@ -1,2 +1,25 @@
-/* Please write a function that returns the product of two numbers */
+import java.util.Scanner;
 
+/* Please write a function that returns the product of two numbers */
+class main {
+
+  public static void main(String[] args) {
+    double first;
+    double second;
+
+    Scanner myObj = new Scanner(System.in);
+    System.out.print("Enter the first number: ");
+
+    first = Double.parseDouble(myObj.nextLine()); // Read user input
+
+    System.out.println();
+
+    System.out.print("Enter the second number: ");
+
+    second = Double.parseDouble(myObj.nextLine()); // Read user input
+
+    System.out.println();
+
+    System.out.println("The product of " + first + " and " + second + " is: " + first * second);
+  }
+}
